0a9beb65227178dc94acb8b94209903ad309d552
Doyle Ravnaas <doylerav@ccs.neu.edu>
2015-10-15 08:34:54 -0700

    tweaks to batch file, added csv file, refreshed zk config to curcfg

--- zkcli-getcfg.bat
+++ zkcli-getcfg.bat
@@ -1,6 +1,2 @@
 set ZKCLI_HOME=%solr_home%\server\scripts\cloud-scripts
-call %solr_home%\server\scripts\cloud-scripts\zkcli -zkhost localhost:9983 -cmd list >tmp.txt
-
-echo - look over the config to find various config names, then...
-echo mkdir temp
-echo %zkcli_home%\zkcli -zkhost localhost:9983 -cmd downconfig -confdir temp -confname <config name>
+call %zkcli_home%\zkcli -zkhost localhost:9983 -cmd list >tmp.txt
