c5eab4b819e01ba1f7002453aa6595d4e3aaa1c2
Doyle Ravnaas <doylerav@ccs.neu.edu>
2015-10-24 09:22:33 -0700

    reworked query response class for SolrJ

--- CrunchifyTutorial/src/com/crunchify/controller/CrunchifyHelloWorld.java
+++ CrunchifyTutorial/src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -1,26 +1,13 @@
 package com.crunchify.controller;
 
-import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.SolrDocumentList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /*
  * original by Crunchify.com
@@ -50,35 +37,35 @@ public class CrunchifyHelloWorld {
 
       solrFormattedMsg = solrFormattedMsg + "Solr response:  ";        
       
-      if (response.responseHeader.status == 0)
+      if (response.header.status == 0)
       {
         solrFormattedMsg = solrFormattedMsg + "<b>Solr response = ok.</b>";
         
         // Build a table of providers:
         String providerList = "";
-        for (ProviderInfo pi : response.response.docs)
+        for (Provider pi : response.body.docs)
         {
           providerList = providerList.concat("<li>");
 
           // First name, last name, type, city, state, hcpcs description
 
-          providerList = providerList.concat(pi.NPPES_PROVIDER_LAST_ORG_NAME);
-          if (!pi.NPPES_PROVIDER_FIRST_NAME.isEmpty())
+          providerList = providerList.concat(pi.last_or_org_name);
+          if (!pi.first_name.isEmpty())
           {
-            providerList = providerList.concat(", " + pi.NPPES_PROVIDER_FIRST_NAME);
+            providerList = providerList.concat(", " + pi.first_name);
           }
-          providerList = providerList.concat(", " + pi.PROVIDER_TYPE);
-          providerList = providerList.concat(", " + pi.NPPES_PROVIDER_CITY);
-          providerList = providerList.concat(", " + pi.NPPES_PROVIDER_STATE);
-          providerList = providerList.concat(", " + pi.HCPCS_DESCRIPTION);
+          providerList = providerList.concat(", " + pi.provider_type);
+          providerList = providerList.concat(", " + pi.city);
+          providerList = providerList.concat(", " + pi.state);
+          providerList = providerList.concat(", " + pi.hcpcs_description);
           
           providerList = providerList.concat("</li>");                   
         }
         
-        if (response.response.numFound > 0)
+        if (response.body.numFound > 0)
         {
-          solrFormattedMsg = solrFormattedMsg + "<br />Found " + response.response.numFound +
-                           " providers for query; first " + response.response.docs.size() + " are...<br />";
+          solrFormattedMsg = solrFormattedMsg + "<br />Found " + response.body.numFound +
+                           " providers for query; first " + response.body.docs.size() + " are...<br />";
           solrFormattedMsg = solrFormattedMsg + "<ul>" + providerList + "</ul>";        
 
         }
@@ -87,7 +74,7 @@ public class CrunchifyHelloWorld {
         }
       }
       else {
-        solrFormattedMsg = solrFormattedMsg + "<b>Solr response not ok = " + response.responseHeader.status + "</b>";
+        solrFormattedMsg = solrFormattedMsg + "<b>Solr response not ok = " + response.header.status + "</b>";
        
       }
       
@@ -100,102 +87,20 @@ public class CrunchifyHelloWorld {
   
   public SolrQueryResponse QuerySolrUrl(String solrUrlBase, String queryTerm) throws Exception
   {
-    String solrQueryUrl = solrUrlBase;
     SolrQueryResponse response = null;        
-    int numRows = 10;
-   
-    SolrClient solr = null;
+    int numRows = 10;      
     
-    try {
-
-      if (!useSolrJ)
-      {
-        // note that some work is needed to get this path working again,
-        // the response objects migrated a bit for solrJ
+      
+    // Hydrate our object from the SolrJ results (maybe this could be a constructor)
+    List<Provider> hits = SolrQueryResponse.getProviders(numRows, queryTerm);
         
-        // Use built http request + json parsing into java objects.
-        solrQueryUrl = solrQueryUrl + "csvtest/select?wt=json&indent=true&q=" + queryTerm
-            + "&rows=" + numRows + "&start=0";
-
-        String rawResponse = GetSolrResponse(solrQueryUrl);
-        response = new SolrQueryResponse(rawResponse);       
-        
-      }
-      else {
-        // Use SolrJ
-        solr = new HttpSolrClient(solrUrlBase + "csvtest");      
-        
-        SolrQuery query = new SolrQuery();
-        
-        query.set("rows", numRows);
-        query.set("q", queryTerm);
-        query.setStart(0);
-        
-        QueryResponse solrJresponse = solr.query(query);
-        
-        SolrDocumentList list = solrJresponse.getResults();
-        
-        // Hydrate our object from the SolrJ results (maybe this could be a constructor)
-        response = new SolrQueryResponse();
-        response.responseHeader.status = solrJresponse.getStatus();
-        response.responseHeader.QTime = solrJresponse.getQTime();
-        response.response.start = list.getStart();
-        response.response.maxScore = list.getMaxScore();
-        response.response.numFound = list.getNumFound();
-        
-        for (int i=0; i<list.size(); i++)
-        {
-          SolrDocument doc = list.get(i);
-          System.out.println("Query result " + i + ": id = " + doc.getFieldValue("id").toString());
-
-          response.response.docs.add(new ProviderInfo(doc.getFieldValueMap()));         
-          
-        }      
-
-      }
-    }    
-    finally {
-      if (solr != null)
-      {
-        solr.close();
-      }
-    }
+    for (Provider p : hits)
+    {
+       System.out.println("Query result : id = " + p.id);         
+    } 
     
     return response;
         
    }
   
-  public static String GetSolrResponse(String solrUrl) throws Exception
-  {
-    String jsonResponse = "";
-    HttpURLConnection conn = null;
-    
-    try {
-      URL url = new URL(solrUrl);
-      conn = (HttpURLConnection) url.openConnection();
-      conn.setRequestMethod("GET");
-      conn.setRequestProperty("Accept", "application/json");
-
-      if (conn.getResponseCode() != 200) {        
-        throw new Exception("Warning: Http response <> 200: "
-            + conn.getResponseCode());
-      }
-
-      BufferedReader br = new BufferedReader(new InputStreamReader(
-        (conn.getInputStream())));
-
-      String output;
-      while ((output = br.readLine()) != null) {
-        jsonResponse = jsonResponse + output;    
-      }
-
-    }
-    finally {
-      if (conn != null)
-      {
-        conn.disconnect();
-      }
-    }
-    return jsonResponse;
-  }
 }
--- JavaPrompt.bat
+++ JavaPrompt.bat
@@ -1,14 +1,24 @@
-rem cmd -k (absolute path to this batch file)
-rem be sure to mark as admin, solr seems to need that
 
-rem download 64 bit 1.8 java, hadoop, solr, spark, maven, solr-spark - put them in c:\program files
-rem set JAVA_HOME=C:\Progra~1\Java\jdk1.7.0_72
-rem set JAVA_Home=c:\progra~1\java\jdk1.8.0_60
+@rem cmd -k (absolute path to this batch file)
+@rem be sure to mark as admin, solr seems to need that
+
+@rem download 64 bit 1.8 java, hadoop, solr, spark, maven, solr-spark 
+@rem set JAVA_HOME=C:\Progra~1\Java\jdk1.7.0_72
+set JAVA_Home=c:\progra~1\java\jdk1.8.0_60
+
 set ANT_HOME=C:\Progra~1\ant\apache-ant-1.9.4
 set SOLR_HOME=c:\OpenSource\solr-5.3.0
 set MAVEN_HOME=c:\OpenSource\apache-maven-3.3.3
 set SPARK_HOME=c:\OpenSource\spark-1.5.0-bin-hadoop2.6
 set HADOOP_HOME=c:\OpenSource\hadoop-2.6.0
+set PERL_HOME=c:\opensource\Strawberry\perl
 
-set PATH=%java_home%\bin;C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;%ant_home%\bin;C:\Python27;c:\progra~1\phantomjs\phantomjs-1.9.8;c:\users\malcolm\AppData\local\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\bin;C:\Progra~1\eclipse;%maven_home%\bin;%hadoop_home%\bin;%spark_home%\bin;%solr_home%\bin;
+set PATH=%java_home%\bin;C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;%ant_home%\bin;C:\Python27;c:\progra~1\phantomjs\phantomjs-1.9.8;%localappdata%\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\bin;C:\Progra~1\eclipse;%maven_home%\bin;%hadoop_home%\bin;%spark_home%\bin;%solr_home%\bin;%perl_home%\bin;
 
+@echo.
+@rem @if NOT /I "%~dp0" == "c:\opensource\" @if exist c:\opensource copy %0 c:\opensource
+@echo.
+
+@if NOT exist c:\opensource @echo Warning - path are set relative to non-existent c:\opensource directory!
+@if .%java_home%. == .. @echo WARNING - you must set java_home!!
+@if NOT exist %java_home%\bin\javac.exe @echo WARNING - javac not found at %java_home%\bin!
